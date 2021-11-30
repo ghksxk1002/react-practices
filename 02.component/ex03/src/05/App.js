082cff27c158f4ecc5c3baa63a4f224d730c2458
ghksxk1002 <ghksxk1002@naver.com>
2021-11-30 13:45:29 +0900

    Scoll 이렇게 하는거 맞나

--- 02.component/ex03/src/05/App.js
+++ 02.component/ex03/src/05/App.js
@@ -5,14 +5,29 @@ export default function App() {
     const outterRef = useRef(null);
     const innerRef = useRef(null);
 
+    const Scoll = () => {
+        let st = outterRef.current.scrollTop;
+        let ot = outterRef.current.clientHeight;
+        let top = innerRef.current.clientHeight;
+
+        if(top == (st+ot)){
+            console.log("닿았다")
+        }
+
+    }
+
     return (
         <div
-            className={'App'}>
+            ref={ outterRef }
+            className={'App'}
+            onScroll={ Scoll } >
             <div
                 ref={ innerRef }>
                 <ul>
-                    { Array.from({length: 100}, (_, i) => i+1).map(i =>
-                        <li>
+                    {/* 객체에다가 유사객체를 만든후 길이는 100, 
+                        그리고 뒤에 map 함수를 써서 1에서 부터 100 까지의 배열을 만든것 */}
+                    { Array.from({length: 100}, (e, i) => i+1).map(i =>
+                        <li key={i}>
                             { `아이템 ${i} 입니다.` }
                         </li>
                     ) }
