15089105e94b4c68edb9b6f8f4eebbcf19b7e6da
ghksxk1002 <ghksxk1002@naver.com>
2021-11-30 11:16:07 +0900

    class 형 컴포넌트 이벤트 처리해보기

--- 02.component/ex03/src/02/TitleBar01.js
+++ 02.component/ex03/src/02/TitleBar01.js
@@ -1,12 +1,42 @@
 import React, {Component} from 'react';
 
+// status 를 직접 관리
 export default class TitelBar01 extends Component {
 
+    // 생성자
+    constructor(){
+        super(...arguments); // 스프레드 연산다 ... 요거 arguments[0], arguments[1] 이렇게 할필요가 없다
+        
+        //this.no = 0;
 
+        this.state = {
+            no:0
+        }
+
+    }
+
+    // 확장 객체 리터럴에서 이렇게 만드는것을 지원한다?
+    onClickHandler() {
+        this.no++;
+        // setState 메소드는 this의 변화를 렌더해준다
+        this.setState({
+            no: this.state.no + 1
+        })
+        console.log('TitleBar01 Clicked');
+    }
 
     render() {
         return (
-
+            <h1
+                // 이 객체의, onClickHandler 를 호출 하겠다는 말, .bind(this) 이 함수를 내가 부르는것이 아닌
+                // DOM이 호출하는 것이기 때문에 미리 그 함수를 바인딩 시켜야한다
+                onClick = { this.onClickHandler.bind(this) }   
+                style={{cursor: 'pointer'}}>
+                ex03 - Funcational Event Handler(Class Component)
+                {/* no:${ this.no },  */}
+                {`state.no:${this.state.no}`} 
+                
+            </h1>
         )
     }
 }
--- 02.component/ex03/src/02/TitleBar02.js
+++ 02.component/ex03/src/02/TitleBar02.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+// Functional 컨포넌트
+// status hock를 통해 리액트에게 관리 넘김
 export default function TitelBar02() {
 
 
     return (
-
+        <h1>ex03 - Funcational Event Handler(Functional Component)</h1>
     )
 }
