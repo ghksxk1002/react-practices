1c278c91869a5c42ab18a1ff5abd7dab277dd672
ghksxk1002 <ghksxk1002@naver.com>
2021-11-30 11:48:47 +0900

    SyntheticEvent 이벤트 합성

--- 02.component/ex03/src/02/TitleBar02.js
+++ 02.component/ex03/src/02/TitleBar02.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Functional 컨포넌트
-// status hock를 통해 리액트에게 관리 넘김
+// status hock 함수를 통해 리액트에게 관리 넘김
 export default function TitelBar02() {
-
+    
+    const onClickHandler = () => { 
+        // 함수 형이기 때문에 this를 사용할수 없다
+        console.log('TitleBar02 Clicked');
+    }
 
     return (
-        <h1>ex03 - Funcational Event Handler(Functional Component)</h1>
+        <h1
+            onClick = { onClickHandler }
+            style={{cursor:"pointer"}}>
+            ex03 - Funcational Event Handler(Functional Component)
+        </h1>
     )
 }
--- 02.component/ex03/src/03/App.js
+++ 02.component/ex03/src/03/App.js
@@ -7,7 +7,7 @@ export default function App() {
     }
 
     const onAddFormSubmit = function(event) {
-        event.preventDefault();
+        event.preventDefault(); //DOM API 인 preventDefault()
         console.log(event.target.name, ':', 'Ajax로 등록할 겁니다.');
     }
 
@@ -22,12 +22,12 @@ export default function App() {
                 name='addForm'
                 method='post'
                 action='/do/not/go'
-                >
+                onSubmit={ onAddFormSubmit }>
                 <input
                     type='text'
                     name='message'
                     placeholder='메세지를 입력 하세요'
-                    />
+                    onChange={ onChangeMessageInput }/>
                 <br/>
                 <br/>
                 <input
