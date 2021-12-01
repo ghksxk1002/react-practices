886e8c6ba82d6bfce4ca53078213ce1ef099a643
ghksxk1002 <ghksxk1002@naver.com>
2021-12-01 12:31:16 +0900

    ...

--- 02.component/ex04/src/03/Form.js
+++ 02.component/ex04/src/03/Form.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './assets/Form.css';
 
+// 통으로 발리데이션을 하고 싶을때
 export default function Form() {
+    const onSubmit = (e) => {
+        e.preventDafault()
 
+        // 태그의 name으로 값을 찾아와서
+        console.log(e.target.email.value, ":", e.target.password.value);
+
+        // ajax 로그인
+    }
 
     return (
         <form
             id="loginForm"
             name="loginForm"
             method="post"
-            action="/do/not/post">
+            action="/do/not/post"
+            onSubmit={onSubmit}> 
             <label htmlFor="email">이메일</label>
             <input
                 id="email"
---
+++ Emaillist/src/App.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react';
+
+export default function(){
+    return (
+        //  게스트북 입력폰과 게스트북 계시글 리스트
+        <div className={styles.App}>
+           
+        </div>
+
+    )
+}
