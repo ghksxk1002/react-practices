38ce8537cbd7984872b38706afe73ca321f32c6f
ghksxk1002 <ghksxk1002@naver.com>
2021-12-01 09:46:44 +0900

    클래스형 스크롤 이벤트 처리

--- 02.component/ex03/src/06/App.js
+++ 02.component/ex03/src/06/App.js
@@ -3,16 +3,24 @@ import './assets/scss/App.scss';
 
 export default class App extends Component {
 
+    onScroll() {
+        console.log(this.OutterRef.scrollTop, ':', this.OutterRef.clientHeight, ":" , this.innerRef.clientHeight)
+    }
+
 
     render() {
         return (
             <div
-                className={'App'}>
+            // outer
+            ref={ ref => this.OutterRef = ref }
+            className={'App'}
+            onScroll={ this.onScroll.bind(this) }> 
                 <div
+                    // inner
                     ref={ (ref) => this.innerRef = ref }>
                     <ul>
                         {Array.from({length: 100}, (_, i) => i + 1).map(i =>
-                            <li>
+                            <li key={i}>
                                 {`아이템 ${i} 입니다.`}
                             </li>
                         )}
