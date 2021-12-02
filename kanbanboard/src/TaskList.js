2ee8a8f4885d08dcb63d411e3849f12bcdb60397
ghksxk1002 <ghksxk1002@naver.com>
2021-12-02 16:27:55 +0900

    칸반보드

--- kanbanboard/src/TaskList.js
+++ kanbanboard/src/TaskList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Task from "./Task";
-
+import styles from "./assets/css/taskList.css"
 export default function({tasks}){
     console.log(tasks)
     return (
@@ -10,6 +10,10 @@ export default function({tasks}){
                                     key={task.no} 
                                     name={task.name}/>)}
             </ul>
+            <input 
+                type='text'
+                className={styles['TaskList--add-task']} 
+                placeholder='태스크 추가'/>
         </div>
 
     )
