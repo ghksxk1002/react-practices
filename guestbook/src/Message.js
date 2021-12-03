6a41410f431b447508efa5cf27c23b3fa815825b
ghksxk1002 <ghksxk1002@naver.com>
2021-12-03 14:38:57 +0900

    guestbook

--- guestbook/src/Message.js
+++ guestbook/src/Message.js
@@ -2,15 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './assets/scss/Message.scss';
 
-export default function Message({no, name, message}) {
+export default function Message({no, name, message, notifyDeleteMessage}) {
     return (
         <li className={styles.Message}>
             <strong>{name}</strong>
             <p>
-                {message}
+                {/* 리엑트 안에서 계행 처리하기 */}
+                { 
+                    message && message.split('\n').map((line,index)=> index>0 ? 
+                        <span key={`${no}-${index}`}>
+                            <br/>
+                            {line}
+                        </span> :
+                        line)
+                }
             </p>
             <strong/>
-            <a>삭제</a>
+            {/* 자식에서 함수를 호출하기 위해 정의를 해주어야 한다 */}
+            <a onClick={()=>notifyDeleteMessage(no)}>삭제</a>
         </li>
     );
 }
