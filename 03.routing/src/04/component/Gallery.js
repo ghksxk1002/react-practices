dd911d1e133a25f09fbcc2ad32278935b940353d
ghksxk1002 <ghksxk1002@naver.com>
2021-12-08 11:45:15 +0900

    link와 NavLink 예제

--- 03.routing/src/04/component/Gallery.js
+++ 03.routing/src/04/component/Gallery.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import {Link, NavLink} from 'react-router-dom';
 
 export default function Gallery() {
     return (
         <div>
             <h1>Gallery</h1>
+            {
+                /*/
+                <ul>
+                    <li><Link to={'/'}>[Main]</Link></li>
+                    <li><Link to={'gallery'}>[gallery]</Link></li>
+                    <li><Link to={'guestbook'}>[guestbook]</Link></li>
+                </ul>
+                /*/
+            }
+            {
+                <ul>
+                    <li><NavLink to={'/'}>[Main]</NavLink></li>
+                    <li><NavLink to={'/gallery'}>[gallery]</NavLink></li>
+                    <li><NavLink to={'/guestbook'}>[guestbook]</NavLink></li>
+                </ul>
+                //*/
+            }
         </div>
     );
 }
--- 03.routing/src/04/component/Guestbook.js
+++ 03.routing/src/04/component/Guestbook.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import {Link, NavLink} from 'react-router-dom';
 
 export default function Guestbook() {
     return (
         <div>
             <h1>Guestbook</h1>
+            {
+                /*/
+                <ul>
+                    <li><Link to={'/'}>[Main]</Link></li>
+                    <li><Link to={'gallery'}>[gallery]</Link></li>
+                    <li><Link to={'guestbook'}>[guestbook]</Link></li>
+                </ul>
+                /*/
+            }
+            {
+                <ul>
+                    <li><NavLink to={'/'}>[Main]</NavLink></li>
+                    <li><NavLink to={'/gallery'}>[gallery]</NavLink></li>
+                    <li><NavLink to={'/guestbook'}>[guestbook]</NavLink></li>
+                </ul>
+                //*/
+            }
         </div>
     );
 }
